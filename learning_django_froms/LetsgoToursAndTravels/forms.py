6affa28191a2be30eaeb7474465939d535a95447
Agnik Roy <Ar12agnik@gmail.com>
2024-07-20 11:33:32 +0530

    update

--- learning_django_froms/LetsgoToursAndTravels/forms.py
+++ learning_django_froms/LetsgoToursAndTravels/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import Tour,agencies,places
 class TourForm(forms.Form):
-    Tour=forms.ModelChoiceField(queryset=Tour.objects.all(),required=True,label='Tour')
+    Tour=forms.ModelChoiceField(queryset=Tour.objects.all(),required=True,label='Tour',widget=forms.RadioSelect)
     Name=forms.CharField(max_length=100,required=True,label='Name')
     phone=forms.IntegerField(required=True,label='Phone')
     address=forms.CharField(required=True,label='Address')
